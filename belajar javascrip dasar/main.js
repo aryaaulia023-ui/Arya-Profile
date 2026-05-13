e4c9b9717b522f7115244713bc8fee021e9cfe29
aryaaulia023-ui <aryaaulia023@gmail.com>
2026-05-13 23:53:17 +0700

    Add files via upload

---
+++ belajar javascrip dasar/main.js
@@ -0,0 +1,8 @@
+// alert("nama saya arya aulia mifta"); //tidak di pakai karena sudah diganti dengan console.log
+// alert("saya berasal dari kota pekanbaru");
+// alert("hobi saya adalah bermain futsal");
+
+console.log("Nama saya arya aulia mifta"); //code untuk menampilkan nama pada output terminal
+console.log("Saya berasal dari kota pekanbaru"); //code untuk menampilkan asal kota pada output terminal
+console.log("Hobi saya adalah bermain futsal"); //code untuk menampilkan hobi pada output terminal
+
